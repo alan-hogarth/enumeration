e5280d0ae3638664ac419a1532c11a80e671c893
Alan Hogarth <alan.hogarth@outlook.com>
2020-12-09 20:16:55 +0000

    completed with extensions

--- models/traveller.js
+++ models/traveller.js
@@ -22,16 +22,34 @@ Traveller.prototype.getJourneysByTransport = function (transport) {
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  })
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  }, 0)
 
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
+  const vehicles = this.journeys.map((journey) => {
+    return journey.transport});
+   
+    let uniqueModes = [];
+    
+    vehicles.forEach((vehicle) => {
+      if (uniqueModes.includes(vehicle) === false) {
+        uniqueModes.push(vehicle);
+      }
+    })
+      return uniqueModes
+    
+  }
 
-};
+
 
 
 module.exports = Traveller;
